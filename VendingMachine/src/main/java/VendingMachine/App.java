8f61ae6ef40f37e1df7916cf4a33b5119adaca04
AbdinurM <87390658+AbdinurM@users.noreply.github.com>
2022-01-03 17:59:42 -0600

    Add files via upload

--- VendingMachine/src/main/java/VendingMachine/App.java
+++ VendingMachine/src/main/java/VendingMachine/App.java
@@ -12,6 +12,8 @@ import UI.UserIOConsoleImpl;
 import UI.VendingMachineView;
 import VendingDao.VendingMachineDao;
 import VendingDao.VendingMachineDaoFileImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -19,12 +21,16 @@ import VendingDao.VendingMachineDaoFileImpl;
  */
 public class App {
         public static void main(String[] args) {
-        
-       UserIO io = new UserIOConsoleImpl();
-      VendingMachineView view = new VendingMachineView(io);
-       VendingMachineDao dao = new VendingMachineDaoFileImpl();
-      VendingMachineServiceImpl service = new VendingMachineServiceImpl(dao);
-      VendingMachineController controller = new VendingMachineController (view, service);
-       controller.run();
+         ApplicationContext ctx
+                = new ClassPathXmlApplicationContext("classpath:applicationContext.xml");
+        VendingMachineController controller
+                = ctx.getBean("controller", VendingMachineController.class);
+        controller.run();
+       //UserIO io = new UserIOConsoleImpl();
+      //VendingMachineView view = new VendingMachineView(io);
+      // VendingMachineDao dao = new VendingMachineDaoFileImpl();
+     // VendingMachineServiceImpl service = new VendingMachineServiceImpl(dao);
+     // VendingMachineController controller = new VendingMachineController (view, service);
+       //controller.run();
 }
 }
